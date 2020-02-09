d6aaac78be96edb1e64396304b5b45e3b133ab1c
Calvin Liang <me@calvin.sh>
2020-02-09 00:16:48 -0800

    Update LocaleChangedReceiver.java

--- app/src/main/java/sh/calvin/langchanger/LocaleChangedReceiver.java
+++ app/src/main/java/sh/calvin/langchanger/LocaleChangedReceiver.java
@@ -8,19 +8,16 @@ import android.content.Intent;
 import android.content.res.Resources;
 
 public class LocaleChangedReceiver extends BroadcastReceiver {
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		
-		if (intent.getAction().compareTo(Intent.ACTION_LOCALE_CHANGED) == 0) {
-			Resources res = context.getResources();
-			if (!res.getConfiguration().locale.getLanguage().equals("en")) {
-				Locale locale = new Locale("en", "US");
-				try {
-					MainActivity.changeLanguage(locale);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Resources res = context.getResources();
+        if (!res.getConfiguration().locale.getLanguage().equals("en")) {
+            Locale locale = new Locale("en", "US");
+            try {
+                MainActivity.changeLanguage(locale);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
